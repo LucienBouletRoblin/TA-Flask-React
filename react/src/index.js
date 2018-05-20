bb14d0bd8f2316c35fbfe27adbc74326e9057647
Raphaël BIDEAU <raphael.bideau@gmail.com>
2018-05-20 14:43:24 +0200

    Add Prettier

--- react/src/index.js
+++ react/src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // In the default template, body is empty. We must
 // create a div to mount the app.
-if(!document.getElementById('app')) {
-  const divElem = document.createElement('div');
-  divElem.id = 'app';
+if (!document.getElementById("app")) {
+  const divElem = document.createElement("div");
+  divElem.id = "app";
   document.body.appendChild(divElem);
 }
 
-const title = 'My Minimal React Webpack Babel Setup !!!';
+const title = "My Minimal React Webpack Babel Setup !!!";
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
 
 module.hot.accept();
