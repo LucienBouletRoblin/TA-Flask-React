961cb7f52067cd4fddd075a1b280f54360263dd6
lucien <lucien.bouletroblin@gmail.com>
2018-05-20 13:41:08 +0200

    modifying docker stuff for hot reload

--- flask/web/app.py
+++ flask/web/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
- 
+
 app = Flask(__name__)
- 
- 
+
+
 @app.route('/')
 def hello_whale():
-    return "Hello there" 
- 
+    return "Hello there"
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
