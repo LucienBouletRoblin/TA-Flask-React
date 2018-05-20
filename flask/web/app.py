59d2077b3b504d3963ab4c7c32e17cdf06e56e62
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2018-05-20 18:40:04 +0200

    adding graphql example with flask coupled to a postgresql

--- flask/web/app.py
+++ flask/web/app.py
@@ -1,12 +1,50 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+from schema import schema
+from models import Department, Employee
+from database import db_session, Base, engine
 
 app = Flask(__name__)
+app.debug = True
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True  # for having the GraphiQL interface
+    )
+)
 
 
-@app.route('/')
-def hello_whale():
-    return "Hello there vq"
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/create-data')
+def hello_world():
+    Base.metadata.create_all(bind=engine)
+    engineering = Department(name='Engineering')
+    db_session.add(engineering)
+    hr = Department(name='Human Resources')
+    db_session.add(hr)
+
+    peter = Employee(name='Peter', department=engineering)
+    db_session.add(peter)
+    roy = Employee(name='Roy', department=engineering)
+    db_session.add(roy)
+    tracy = Employee(name='Tracy', department=hr)
+    db_session.add(tracy)
+    db_session.commit()
+
+    return 'Data created, hopefully'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
