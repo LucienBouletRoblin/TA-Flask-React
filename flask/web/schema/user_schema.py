1885eed3b4051497a85dc2cb11542b3463d89746
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2018-05-21 19:59:01 +0200

    adding logger example in schema_user and changinglastnameuser example

--- flask/web/schema/user_schema.py
+++ flask/web/schema/user_schema.py
@@ -3,6 +3,10 @@ from graphene_sqlalchemy import SQLAlchemyObjectType
 from database import db_session
 from models.user import User as UserModel
 
+import logging
+
+log = logging.getLogger('custom_logger')
+
 
 class User(SQLAlchemyObjectType):
     class Meta:
@@ -39,5 +43,25 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user, ok=ok)
 
 
+class ChangeUserLastName(graphene.Mutation):
+    class Arguments:
+        last_name = graphene.String()
+        id = graphene.Int()
+
+    ok = graphene.Boolean()
+    user = graphene.Field(lambda: User)
+
+    def mutate(self, info, last_name, id):
+        log.info('---------------------------------------------')
+        log.info(id)
+        user = UserModel.query.get(id)
+        log.info(user)
+        user.last_name = last_name
+        db_session.commit()
+        ok = True
+        return ChangeUserLastName(user=user, ok=ok)
+
+
 class UserMutations(graphene.ObjectType):
     create_user = CreateUser.Field()
+    change_user_last_name = ChangeUserLastName.Field()
